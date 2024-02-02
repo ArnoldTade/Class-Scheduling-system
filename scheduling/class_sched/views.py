412ecaa628e3c79b8859f0d6cc0bd4789f063a82
Arnold Saldivar <saldivararnold80@gmail.com>
2024-02-02 19:52:49 +0800

    Profile being edited

--- scheduling/class_sched/views.py
+++ scheduling/class_sched/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from .forms import UserCreationForm, LoginForm
 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -33,7 +33,7 @@ def user_login(request):
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
-                return redirect("home")
+                return redirect("profile")
     else:
         form = LoginForm()
     return render(request, "login.html", {"form": form})
@@ -52,6 +52,7 @@ def home(request):
     return render(request, "home.html")
 
 
+@login_required
 def profile(request):
     return render(request, "profile.html")
 
